1bd4ac9f667df954b3149791815bf9c4935c0e43
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-10-14 20:54:15 +0400

    [ios] add new iinitializer to the MWMButtonCell
    This initializer allows to configure the button and use the block for actions instead of delegates.
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/Editor/Cells/MWMButtonCell.h
+++ iphone/Maps/UI/Editor/Cells/MWMButtonCell.h
@@ -11,6 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface MWMButtonCell : MWMTableViewCell
 
 - (void)configureWithDelegate:(id<MWMButtonCellDelegate>)delegate title:(NSString *)title enabled:(BOOL)enabled;
+- (void)configureWithTitle:(NSString *)title styleName:(NSString *)styleName action:(MWMVoidBlock _Nullable)action;
 
 @end
 
--- iphone/Maps/UI/Editor/Cells/MWMButtonCell.m
+++ iphone/Maps/UI/Editor/Cells/MWMButtonCell.m
@@ -1,9 +1,11 @@
 #import "MWMButtonCell.h"
+#import "SwiftBridge.h"
 
 @interface MWMButtonCell ()
 
 @property(nonatomic) IBOutlet UIButton *button;
 @property(weak, nonatomic) id<MWMButtonCellDelegate> delegate;
+@property(nonatomic, nullable) MWMVoidBlock action;
 
 @end
 
@@ -15,8 +17,18 @@
   self.delegate = delegate;
 }
 
+- (void)configureWithTitle:(NSString *)title styleName:(NSString *)styleName action:(MWMVoidBlock)action
+{
+  [self.button setTitle:title forState:UIControlStateNormal];
+  self.action = action;
+  [self.button addTarget:self action:@selector(buttonTap) forControlEvents:UIControlEventTouchUpInside];
+  [self.button setStyleName:styleName];
+}
+
 - (IBAction)buttonTap {
   [self.delegate cellDidPressButton:self];
+  if (self.action)
+    self.action();
 }
 
 @end
