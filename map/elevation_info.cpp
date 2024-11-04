17696100111edc8ef5f8d53bced79cff96e979d6
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-11-04 13:11:46 +0400

    [map] refactor ElevationInfo
    1. filling with data properties is removed because there is no such data for tracks
    2. elevation profile is initialized only with the MultiGeometry because it shouldn't know about any other info exept the lengths, altitudes and timestamps
    3. data is calculated as concatenation of all geometry segments (not the 1st one as in old implementation)
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- map/elevation_info.cpp
+++ map/elevation_info.cpp
@@ -2,70 +2,49 @@
 
 #include "geometry/mercator.hpp"
 
-#include "base/string_utils.hpp"
-
-namespace
+ElevationInfo::ElevationInfo(kml::MultiGeometry const & geometry)
 {
-static uint8_t constexpr kMaxDifficulty = ElevationInfo::Difficulty::Hard;
-
-std::string const kAscentKey = "ascent";
-std::string const kDescentKey = "descent";
-std::string const kLowestPointKey = "lowest_point";
-std::string const kHighestPointKey = "highest_point";
-std::string const kDifficultyKey = "difficulty";
-std::string const kDurationKey = "duration";
-
-template <typename T>
-void FillProperty(kml::Properties const & properties, std::string const & key, T & value)
-{
-  auto const it = properties.find(key);
-  if (it == properties.cend())
-    LOG(LERROR, ("Property not found for key:", key));
-  else
-  {
-    if (!strings::to_any(it->second, value))
-      LOG(LERROR, ("Conversion is not possible for key", key, "string representation is", it->second));
-  }
-}
-}  // namespace
-
-ElevationInfo::ElevationInfo(Track const & track)
-  : m_id(track.GetId())
-  , m_name(track.GetName())
-{
-  // (Distance, Elevation) chart doesn't have a sence for multiple track's geometry.
-  auto const & points = track.GetSingleGeometry();
-  if (points.empty())
-    return;
-
-  m_points.reserve(points.size());
-  m_points.emplace_back(0, points[0].GetAltitude());
   double distance = 0.0;
-  for (size_t i = 1; i < points.size(); ++i)
+  
+  // Concatenate all segments.
+  for (size_t i = 0; i < geometry.m_lines.size(); ++i)
   {
-    distance += mercator::DistanceOnEarth(points[i - 1].GetPoint(), points[i].GetPoint());
-    m_points.emplace_back(distance, points[i].GetAltitude());
+    auto const & points = geometry.m_lines[i];
+    if (points.empty())
+      return;
+
+    double segmentStartDistance = 0.0;
+    if (i == 0)
+    {
+      auto const & baseAltitude = points[i].GetAltitude();
+      m_minAltitude = baseAltitude;
+      m_maxAltitude = baseAltitude;
+    }
+    else
+      segmentStartDistance = m_points.back().m_distance;
+
+    m_segmentDistances.emplace_back(segmentStartDistance);
+    m_points.emplace_back(points[i], segmentStartDistance);
+
+    for (size_t j = 1; j < points.size(); ++j)
+    {
+      distance += mercator::DistanceOnEarth(points[j - 1].GetPoint(), points[j].GetPoint());
+      m_points.emplace_back(points[j], distance);
+
+      auto const & previousPointAltitude = points[j - 1].GetAltitude();
+      auto const & currentPointAltitude = points[j].GetAltitude();
+      auto const deltaAltitude = currentPointAltitude - previousPointAltitude;
+      if (deltaAltitude > 0)
+        m_ascent += deltaAltitude;
+      else
+        m_descent -= deltaAltitude;
+
+      if (currentPointAltitude < m_minAltitude)
+        m_minAltitude = currentPointAltitude;
+      if (currentPointAltitude > m_maxAltitude)
+        m_maxAltitude = currentPointAltitude;
+    }
   }
-
-  auto const & properties = track.GetData().m_properties;
-
-  FillProperty(properties, kAscentKey, m_ascent);
-  FillProperty(properties, kDescentKey, m_descent);
-  FillProperty(properties, kLowestPointKey, m_minAltitude);
-  FillProperty(properties, kHighestPointKey, m_maxAltitude);
-
-  uint8_t difficulty;
-  FillProperty(properties, kDifficultyKey, difficulty);
-
-  if (difficulty > kMaxDifficulty)
-  {
-    LOG(LWARNING, ("Invalid difficulty value", m_difficulty, "in track", track.GetName()));
-    m_difficulty = Difficulty ::Unknown;
-  }
-  else
-  {
-    m_difficulty = static_cast<Difficulty>(difficulty);
-  }
-
-  FillProperty(properties, kDurationKey, m_duration);
+  /// @todo(KK) Implement difficulty calculation.
+  m_difficulty = Difficulty::Unknown;
 }
